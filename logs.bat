f45c680087b0b8ef75396f9d893d9fe4a637b567
sebnar <gomeztovars@gmail.com>
2025-10-06 20:15:36 -0500

    clean resources

--- logs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 📝 Mostrando logs de Servicios Store...
-echo.
-
-docker logs -f servicios-store
-
-pause
--- logs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "📝 Mostrando logs de Servicios Store..."
-echo
-
-docker logs -f servicios-store
